7b3ffbb62ecd24cafff2ff291780797e12d14d3d
phuchieu <phuchieu@lifetek.vn>
2023-11-10 17:12:14 +0700

    update index.js cua component SeachBox

--- app/components/SearchBox/index.js
+++ app/components/SearchBox/index.js
@@ -1,60 +1,80 @@
-import React, { useEffect, useState, useRef } from 'react';
-import {View, Text, TextInput} from 'react-native';
-import { Button } from 'native-base';
-import Icon from 'react-native-vector-icons/MaterialIcons'
-import { Keyboard, Platform } from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 
-const SearchBox = (props) => {
-    const { onChange, setIsSearching, hideCancel } = props;
-    const [textSearch, setTextSearch] = useState('');
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Keyboard, Platform} from 'react-native';
 
-    const searching = useRef();
+const SearchBox = props => {
+  const {onChange, setIsSearching, hideCancel} = props;
+  const [textSearch, setTextSearch] = useState('');
 
-    useEffect(() => {
-        return () => {
-            if (searching.current) clearTimeout(searching.current);
-        };
-    }, []);
+  const searching = useRef();
 
-    const handleSearch = (newText) => {
-        let text = newText
-        text = text.trimLeft()
-
-        if (text === textSearch) return
-
-        setTextSearch(text);
-        if (searching.current) clearTimeout(searching.current);
-        searching.current = setTimeout(() => {
-            if (onChange) onChange(text);
-        }, 500);
-
+  useEffect(() => {
+    return () => {
+      if (searching.current) clearTimeout(searching.current);
     };
+  }, []);
 
-    const onClose = () => {
-        setIsSearching && setIsSearching(false)
-        if (searching.current) clearTimeout(searching.current);
-        onChange('')
-    }
+  const handleSearch = newText => {
+    let text = newText;
+    text = text.trimLeft();
 
-    const onRemove = () => {
-        if (textSearch.trim()) handleSearch('')
-        else setTextSearch('')
-    }
+    if (text === textSearch) return;
 
-    return (
-        <View style={{ position: 'absolute', width: '95%', zIndex: 20, backgroundColor: '#fff', alignSelf: 'center', marginTop: Platform.OS === 'ios' ? 28 : 8, height: 40 }}>
-            <Button transparent onPress={() => onChange(textSearch)}>
-                <Icon name="search" />
-            </Button>
-            <TextInput placeholder="Tìm kiếm" value={textSearch} onChangeText={handleSearch} onSubmitEditing={Keyboard.dismiss} />
-            <Button iconLeft transparent onPress={onRemove}>
-                <Icon name="remove" type="MaterialIcons" />
-            </Button>
-            <Button transparent onPress={onClose}>
-                <Text>Đóng</Text>
-            </Button>
-        </View >
-    );
+    setTextSearch(text);
+    if (searching.current) clearTimeout(searching.current);
+    searching.current = setTimeout(() => {
+      if (onChange) onChange(text);
+    }, 500);
+  };
+
+  const onClose = () => {
+    setIsSearching && setIsSearching(false);
+    if (searching.current) clearTimeout(searching.current);
+    onChange('');
+  };
+
+  const onRemove = () => {
+    console.log('sssss');
+    if (textSearch.trim()) handleSearch('');
+    else setTextSearch('');
+  };
+
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        width: '95%',
+        zIndex: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        alignSelf: 'center',
+        marginTop: Platform.OS === 'ios' ? 28 : 8,
+        height: 40,
+      }}>
+      <TouchableOpacity onPress={() => onChange(textSearch)} style={{paddingHorizontal: 10,fontSize:18}}>
+        <Icon name="search" color="rgba(46, 149, 46, 1)" size={20} />
+      </TouchableOpacity>
+      <TextInput
+        placeholder="Tìm kiếm"
+        value={textSearch}
+        onChangeText={handleSearch}
+        style={{flex: 1}}
+        onSubmitEditing={Keyboard.dismiss}
+      />
+      <TouchableOpacity>
+        <Icon name="remove" type="MaterialIcons" onPress={onRemove} color="rgba(46, 149, 46, 1)" size={20} />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={{paddingHorizontal: 10,}} onPress={onClose}>
+          <Text style={{color:"rgba(46, 149, 46, 1)"}} >ĐÓNG</Text>
+        </View>
+      </TouchableOpacity>
+     
+    </View>
+  );
 };
 
 export default SearchBox;
