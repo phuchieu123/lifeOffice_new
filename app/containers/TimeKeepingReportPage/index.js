62b2dc2bdb6b61dd2acd8c26e439ff3090d90456
phuchieu <phuchieu@lifetek.vn>
2023-11-22 11:40:32 +0700

    TimeKeepingReportPage

--- app/containers/TimeKeepingReportPage/index.js
+++ app/containers/TimeKeepingReportPage/index.js
@@ -2,17 +2,8 @@ import React, { memo } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
-import { View, Text} from "react-native";
-import {
-  Body,
-  Container,
-  Content,
-  Icon,
-  List,
-  ListItem,
-  Right,
-  
-} from 'native-base';
+import { View, Text , TouchableOpacity} from "react-native";
+import Icon from 'react-native-vector-icons/Entypo';
 import { useInjectReducer } from '../../utils/injectReducer';
 import { useInjectSaga } from '../../utils/injectSaga';
 import BackHeader from '../../components/Header/BackHeader';
@@ -33,40 +24,40 @@ const TimeKeepingReportPage = (props) => {
       <BackHeader title="Báo cáo chấm công" navigation={navigation} />
       <View style={styles.content}>
         <View style={styles.content}>
-          <List>
-            <ListItem onPress={() => navigation.navigate('TimeKeepingChart')}>
-              <Body>
-                <Text>Báo cáo nhân sự</Text>
-              </Body>
-              <Right>
-                <Icon name="chevron-right" type="Entypo" />
-              </Right>
-            </ListItem>
-            <ListItem onPress={() => navigation.navigate('EquipmentChart')}>
-              <Body>
-                <Text>Thiết bị</Text>
-              </Body>
-              <Right>
-                <Icon name="chevron-right" type="Entypo" />
-              </Right>
-            </ListItem>
-            <ListItem onPress={() => navigation.navigate('LateEarlyChart')}>
-              <Body>
-                <Text>Chấm công trễ sớm</Text>
-              </Body>
-              <Right>
-                <Icon name="chevron-right" type="Entypo" />
-              </Right>
-            </ListItem>
-            <ListItem onPress={() => navigation.navigate('NoTimeKeepingChart')}>
-              <Body>
-                <Text>Không chấm công</Text>
-              </Body>
-              <Right>
-                <Icon name="chevron-right" type="Entypo" />
-              </Right>
-            </ListItem>
-          </List>
+          <View>
+            <TouchableOpacity style={{padding: 10, backgroundColor: "rgba(46, 149, 46, 1)", borderRadius: 10, flexDirection: 'row', justifyContent:'space-between',margin: 5, alignItems:'center'}} onPress={() => navigation.navigate('TimeKeepingChart')}>
+              <View>
+                <Text style={{color: '#fff'}} >Báo cáo nhân sự</Text>
+              </View>
+              <View>
+                <Icon color='#fff' name="chevron-right" type="Entypo" />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{padding: 10, backgroundColor: "rgba(46, 149, 46, 1)", borderRadius: 10, flexDirection: 'row', justifyContent:'space-between',margin: 5,alignItems:'center'}} onPress={() => navigation.navigate('EquipmentChart')}>
+              <View>
+                <Text style={{color: '#fff'}}>Thiết bị</Text>
+              </View>
+              <View>
+                <Icon  color='#fff' name="chevron-right" type="Entypo" />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{padding: 10, backgroundColor: "rgba(46, 149, 46, 1)", borderRadius: 10, flexDirection: 'row', justifyContent:'space-between',margin: 5,alignItems:'center'}} onPress={() => navigation.navigate('LateEarlyChart')}>
+              <View>
+                <Text style={{color: '#fff'}}>Chấm công trễ sớm</Text>
+              </View>
+              <View>
+                <Icon  color='#fff' name="chevron-right" type="Entypo" />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{padding: 10, backgroundColor: "rgba(46, 149, 46, 1)", borderRadius: 10, flexDirection: 'row', justifyContent:'space-between',margin: 5,alignItems:'center'}} onPress={() => navigation.navigate('NoTimeKeepingChart')}>
+              <View>
+                <Text style={{color: '#fff'}}>Không chấm công</Text>
+              </View>
+              <View>
+                <Icon  color='#fff' name="chevron-right" type="Entypo" />
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
